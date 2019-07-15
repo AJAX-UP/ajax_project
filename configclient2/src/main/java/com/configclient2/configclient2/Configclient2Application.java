a44eea5486caa069d5924d3f20819cc76b022c1b
陈光杰 <chenguangjie@testdaily.cn>
2019-07-15 17:30:57 +0800

    添加分布式配置组件

--- configclient2/src/main/java/com/configclient2/configclient2/Configclient2Application.java
+++ configclient2/src/main/java/com/configclient2/configclient2/Configclient2Application.java
@@ -1,13 +1,23 @@
 package com.configclient2.configclient2;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Configclient2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Configclient2Application.class, args);
     }
 
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
 }

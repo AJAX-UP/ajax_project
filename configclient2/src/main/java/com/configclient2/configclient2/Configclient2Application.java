6bf96013ca15ff68c40f1e5ff35b39e30149f463
陈光杰 <chenguangjie@testdaily.cn>
2019-07-16 14:53:22 +0800

    添加分布式配置组件

--- configclient2/src/main/java/com/configclient2/configclient2/Configclient2Application.java
+++ configclient2/src/main/java/com/configclient2/configclient2/Configclient2Application.java
@@ -3,10 +3,13 @@ package com.configclient2.configclient2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
 public class Configclient2Application {
 
@@ -16,8 +19,9 @@ public class Configclient2Application {
 
     @Value("${foo}")
     String foo;
+
     @RequestMapping(value = "/hi")
     public String hi(){
-        return foo;
+        return this.foo;
     }
 }

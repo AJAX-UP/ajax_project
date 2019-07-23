1424ad470474d02b953af2a9baa5c1dc0a6bb2e9
陈光杰 <chenguangjie@testdaily.cn>
2019-07-23 17:08:19 +0800

    添加链路追踪

--- zipkinserver/src/main/java/com/zipkinserver/zipkinserver/ZipkinserverApplication.java
+++ zipkinserver/src/main/java/com/zipkinserver/zipkinserver/ZipkinserverApplication.java
@@ -2,10 +2,12 @@ package com.zipkinserver.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.EnableZipkinServer;
 
 
 @EnableZipkinServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ZipkinserverApplication {
 
